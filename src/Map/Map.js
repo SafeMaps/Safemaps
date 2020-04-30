c18e2816553b01a26b0328b4515ce102ba6bdd6a
christiancastro1 <ccastro002@citymail.cuny.edu>
2020-04-30 13:55:00 -0400

    Location working
    Changed structured, of Search. Location working

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,35 +1,41 @@
-//https://github.com/react-native-community/react-native-maps  see more info
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import MapView from 'react-native-maps';
-import Search from './Search';
+import MapView, {Marker} from 'react-native-maps';
+import Search from './Search'
+import Geolocation from '@react-native-community/geolocation';
 
 export default class Map extends Component {
-  constructor(props) {
-    super(props);
-  }
-  reset = () => {
-    this.props.navigation.navigate('Home');
-  };
-  render() {
+   constructor(props) {
+        super(props);
+        this.state = {latitude: 0, longitude: 0, longitudeDelta: 0.0143, latitudeDelta: 0.0134,};
+    }
+    componentDidMount() {
+
+        Geolocation.getCurrentPosition(position => {
+            this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude});},
+            error =>{console.log('Error getting location:', error.message)},
+            {enableHighAccuracy:true, maximumAge:1000, timeout:200})
+    }
+
+    render() {
     return (
       <View style={{flex: 1}}>
         <MapView
           style={{flex: 1}}
           showsUserLocation={true}
-          region={{
-            latitude: 40.78343,
-            longitude: -73.96625,
-            longitudeDelta: 0.0143,
-            latitudeDelta: 0.0134,
-          }}
+          region={this.state}
           loadingEnabled={true}
         />
-
+        <Marker coordinate = {this.state}/>
+          {console.log('This is my coordinates', this.state)}
         <Search />
+
       </View>
     );
   }
 }
 
 //const styles = StyleSheet.create({});
+
--- src/Map/Search.js
+++ src/Map/Search.js
@@ -1,27 +1,31 @@
 import React, {Component} from 'react';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
-import Cred from './key';
+import {Cred} from './key';
+import {StyleSheet} from 'react-native'
 
 export default class Search extends Component {
   constructor(props) {
     super(props);
-    const cred = new Cred(); // wil be in git ignore to make sure key is not public
+    const cred = new Cred; // wil be in git ignore to make sure key is not public
     this.state = {key: cred.getKey()};
   }
   render() {
     return (
-      <GooglePlacesAutocomplete
-        placeholder="Where to ?"
-        onPress={(data, details) => {
-          console.log(data, details);
-        }}
+      <GooglePlacesAutocomplete placeholder="Where to ?" onPress={(data, details) => {console.log(data, details);}}
         placeholderTextColor="#333"
         query={{key: this.state.key, language: 'en'}} //cred.key}}
         textInputProps={{autoCapitalize: 'none', autoCorrect: false}}
         enablePoweredByContainer={false} // dont want the powered by google
-        styles={{
-          container: {position: 'absolute', top: 60, width: '100%'},
-          textInputContainer: {
+        styles={{container:styles.container,
+          textInputContainer:styles.textInputContainer ,
+          textInput: styles.textInput ,
+          listView:  styles.listView}}/>
+    );
+  }
+}
+const styles = StyleSheet.create({
+    container: {position: 'absolute', top: 60, width: '100%'},
+    textInputContainer: {
             flex: 1,
             backgroundColor: 'transparent',
             height: 54,
@@ -29,7 +33,7 @@ export default class Search extends Component {
             borderTopWidth: 0, // so the top line does not show
             borderBottomWidth: 0, // bottom line disapers
           },
-          textInput: {
+    textInput: {
             height: 54,
             margin: 0,
             borderRadius: 0,
@@ -40,16 +44,12 @@ export default class Search extends Component {
             shadowColor: '#000',
             fontSize: 18,
           },
-          listView: {
+    listView: {
             borderWidth: 1,
             borderColor: '#DDD',
             backgroundColor: '#fff',
             marginHorizontal: 20,
             marginTop: 20,
-          },
-        }}
-      />
-    );
-  }
-}
-//const styles = StyleSheet.create({});
+          }
+
+});
